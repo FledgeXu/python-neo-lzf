bfe59c9a154c2498f2a88f5ab04cf665d01452f3
Fledge Shiu <xzk0701@gmail.com>
2025-06-20 17:32:19 +0800

    Drop python2 support and using morden build system.

--- lzf_module.c
+++ lzf_module.c
@@ -1,135 +1,103 @@
 #define PY_SSIZE_T_CLEAN
-
 #include "Python.h"
-#include <errno.h>
 #include "lzf.h"
+#include <errno.h>
 
-#if PY_MAJOR_VERSION >= 3
-    #define PYBYTES_FSAS PyBytes_FromStringAndSize
-    #define PyInt_CheckExact(pyobj) 0
-    #define PyInt_AsLong(pyobj) 0
-#else
-    #define PYBYTES_FSAS PyString_FromStringAndSize
-#endif
+#define PYBYTES_FSAS PyBytes_FromStringAndSize
 
-#if PY_MAJOR_VERSION == 2 && PY_MINOR_VERSION <= 4
-    #define Py_ssize_t ssize_t
-#endif
+static PyObject *python_compress(PyObject *self, PyObject *args) {
+  char *input, *output;
+  Py_ssize_t inlen;
+  PyObject *pyoutlen = Py_None;
+  long outlen;
+  PyObject *result;
 
-static PyObject *
-python_compress(PyObject *self, PyObject *args) {
-    char *input, *output;
-    Py_ssize_t inlen;
-    PyObject *pyoutlen = Py_None;
-    long outlen;
-    PyObject *result;
+  if (!PyArg_ParseTuple(args, "s#|O", &input, &inlen, &pyoutlen))
+    return NULL;
 
-    if (!PyArg_ParseTuple(args, "s#|O", &input, &inlen, &pyoutlen))
-        return NULL;
+  if (pyoutlen == Py_None) {
+    outlen = inlen - 1;
+  } else if (PyLong_CheckExact(pyoutlen)) {
+    outlen = PyLong_AsLong(pyoutlen);
+  } else {
+    PyErr_SetString(PyExc_TypeError, "max_len must be an integer");
+    return NULL;
+  }
 
-    if (pyoutlen == Py_None)
-        outlen = inlen - 1;
-    else if (PyInt_CheckExact(pyoutlen))
-        outlen = PyInt_AsLong(pyoutlen);
-    else if (PyLong_CheckExact(pyoutlen))
-        outlen = PyLong_AsLong(pyoutlen);
-    else {
-        PyErr_SetString(PyExc_TypeError, "max_len must be an integer");
-        return NULL;
-    }
+  if (inlen == 1)
+    outlen++; // workaround for liblzf
+  if (outlen <= 0) {
+    PyErr_SetString(PyExc_ValueError, "max_len must be > 0");
+    return NULL;
+  }
 
-    if (inlen == 1) outlen++; /* work around for what looks like a liblzf bug */
-    if (outlen <= 0) {
-        PyErr_SetString(PyExc_ValueError, "max_len must be > 0");
-        return NULL;
-    }
+  output = (char *)malloc(outlen + 1);
+  if (!output) {
+    PyErr_SetString(PyExc_MemoryError, "out of memory");
+    return NULL;
+  }
 
-    output = (char *)malloc(outlen + 1);
-    if (output == NULL) {
-        PyErr_SetString(PyExc_MemoryError, "out of memory");
-        return NULL;
-    }
-    outlen = lzf_compress(input, inlen, output, outlen + 1);
+  outlen = lzf_compress(input, inlen, output, outlen + 1);
 
-    if (outlen)
-        result = PYBYTES_FSAS(output, outlen);
-    else {
-        Py_XINCREF(Py_None);
-        result = Py_None;
-    }
-    free(output);
+  if (outlen)
+    result = PYBYTES_FSAS(output, outlen);
+  else {
+    Py_XINCREF(Py_None);
+    result = Py_None;
+  }
 
-    return result;
+  free(output);
+  return result;
 }
 
-static PyObject *
-python_decompress(PyObject *self, PyObject *args) {
-    char *input, *output;
-    Py_ssize_t inlen;
-    long outlen;
-    PyObject *result;
+static PyObject *python_decompress(PyObject *self, PyObject *args) {
+  char *input, *output;
+  Py_ssize_t inlen;
+  long outlen;
+  PyObject *result;
 
-    if (!PyArg_ParseTuple(args, "s#l", &input, &inlen, &outlen))
-        return NULL;
+  if (!PyArg_ParseTuple(args, "s#l", &input, &inlen, &outlen))
+    return NULL;
 
-    if (outlen < 0) {
-        PyErr_SetString(PyExc_ValueError, "max_len cannot be less than 0");
-        return NULL;
+  if (outlen < 0) {
+    PyErr_SetString(PyExc_ValueError, "max_len cannot be less than 0");
+    return NULL;
+  }
+
+  output = (char *)malloc(outlen);
+  if (!output) {
+    PyErr_SetString(PyExc_MemoryError, "out of memory");
+    return NULL;
+  }
+
+  outlen = lzf_decompress(input, inlen, output, outlen);
+
+  if (outlen)
+    result = PYBYTES_FSAS(output, outlen);
+  else {
+    if (errno == EINVAL) {
+      PyErr_SetString(PyExc_ValueError, "error in compressed data");
+      free(output);
+      return NULL;
     }
+    Py_XINCREF(Py_None);
+    result = Py_None;
+  }
 
-    output = (char *)malloc(outlen);
-    outlen = lzf_decompress(input, inlen, output, outlen);
-
-    if (outlen)
-        result = PYBYTES_FSAS(output, outlen);
-    else {
-        if (errno == EINVAL) {
-            PyErr_SetString(PyExc_ValueError, "error in compressed data");
-            free(output);
-            return NULL;
-        }
-        Py_XINCREF(Py_None);
-        result = Py_None;
-    }
-    free(output);
-
-    return result;
+  free(output);
+  return result;
 }
-
 
 static PyMethodDef methods[] = {
     {"compress", python_compress, METH_VARARGS,
-"compress(input, max_length=None)\n\
-\n\
-return the compressed string, or None if it doesn't compress to smaller \n\
-than the original\n\
-\n\
-takes an optional second parameter, for specifying a maximum compressed \n\
-length (default is one less than the length of the original)"},
+     "compress(input, max_length=None)\n\nReturn the compressed string, or "
+     "None if it doesn't compress smaller."},
     {"decompress", python_decompress, METH_VARARGS,
-"decompress(input, max_length)\n\
-\n\
-return the decompressed string\n\
-\n\
-will return None if the string doesn't decompress to within max_length bytes"},
-    {NULL, NULL, 0, NULL}
-};
+     "decompress(input, max_length)\n\nReturn the decompressed string, or "
+     "raise error on failure."},
+    {NULL, NULL, 0, NULL}};
 
-#if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef lzfmodule = {
-    PyModuleDef_HEAD_INIT,
-    "lzf", "", -1, methods,
-    NULL, NULL, NULL, NULL
-};
+    PyModuleDef_HEAD_INIT, "lzf", NULL, -1, methods, NULL, NULL, NULL, NULL};
 
-PyMODINIT_FUNC
-PyInit_lzf(void) {
-    PyObject *module = PyModule_Create(&lzfmodule);
-    return module;
-}
-#else
-PyMODINIT_FUNC
-initlzf(void) {
-    Py_InitModule("lzf", methods);
-}
-#endif
+PyMODINIT_FUNC PyInit_lzf(void) { return PyModule_Create(&lzfmodule); }
--- setup.py
+++ setup.py
@@ -1,27 +1,41 @@
-from setuptools import setup, Extension
+import sys
 
+from setuptools import Extension, setup
 
-VERSION = (0, 2, 6)
+VERSION = (0, 3, 0)
+
+compile_args = ["/W3"] if sys.platform == "win32" else ["-Wall"]
 
 setup(
-    name="python-lzf",
-    description="C Extension for liblzf",
-    version=".".join(filter(None, map(str, VERSION))),
-    author="Travis Parker",
-    author_email="travis.parker@gmail.com",
-    url="http://github.com/teepark/python-lzf",
-    license="BSD",
-    ext_modules=[Extension(
-        'lzf',
-        ['lzf_module.c', 'lzf_c.c', 'lzf_d.c'],
-        include_dirs=('.',),
-        extra_compile_args=['-Wall'])],
-    classifiers = [
-        "Development Status :: 4 - Beta",
+    name="python-neo-lzf",
+    description="A fork of python-lzf with pre-built wheel files.",
+    version=".".join(map(str, VERSION)),
+    author="Fledge Shiu",
+    author_email="xzk0701@gmail.com",
+    url="https://github.com/FledgeXu/python-neo-lzf",
+    ext_modules=[
+        Extension(
+            "lzf",
+            ["lzf_module.c", "lzf_c.c", "lzf_d.c"],
+            include_dirs=["."],
+            extra_compile_args=compile_args,
+        )
+    ],
+    license="BSD-3-Clause",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
         "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: C",
         "Topic :: System :: Archiving :: Compression",
-    ]
+    ],
+    python_requires=">=3.7",
 )
